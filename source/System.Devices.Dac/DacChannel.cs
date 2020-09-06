387fd98601ac48b8e6d5f17c45ea635b2a61c17f
Robin Jones <networkfusion@users.noreply.github.com>
2020-09-06 22:37:27 +0100

    testing changes for other repos

--- source/System.Devices.Dac/DacChannel.cs
+++ source/System.Devices.Dac/DacChannel.cs
@@ -1,5 +1,6 @@
 ﻿//
 // Copyright (c) 2019 The nanoFramework project contributors
+// Copyright (c) 2020 .NET Foundation. All rights reserved.  
 // See LICENSE file in the project root for full license information.
 //
 
